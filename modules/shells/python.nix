17b52bde8e4735ef747a6e36548e42beb9f1898e
arcanumx64 <195826529+arcanumx64@users.noreply.github.com>
2025-02-24 22:13:18 +0530

    (update):nix flake

--- modules/shells/python.nix
+++ modules/shells/python.nix
@@ -1,8 +1,8 @@
 # python.nix
 {pkgs ? import <nixpkgs> {}}: {
   devShell = pkgs.mkShell {
-    buildInputs = with pkgs.python311Packages; [
-      pkgs.python311
+    buildInputs = with pkgs.python312Packages; [
+      pkgs.python312
       virtualenv
       pip
       setuptools
